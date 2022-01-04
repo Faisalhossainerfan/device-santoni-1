f6637db09e300008e8249eb37853b454e0573a48
Faisalhossainerfan <67220051+Faisalhossainerfan@users.noreply.github.com>
2022-01-04 12:50:32 +0600

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,8 +1,8 @@
-# PixelPlus Ui
+# SparkOS
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_santoni.mk
+    $(LOCAL_DIR)/spark_santoni.mk
 
 COMMON_LUNCH_CHOICES := \
-    aosp_santoni-userdebug \
-    aosp_santoni-user \
-    aosp_santoni-eng
+    spark_santoni-userdebug \
+    spark_santoni-user \
+    spark_santoni-eng
